591011319223d632ffa428cad6574402b8573f95
amelmnd <amelmnd.dev@gmail.com>
2023-06-08 14:49:23 +0200

    udemy ex : first exercise

---
+++ Le_C_par_la_pratique/1-les-bases_exercice/1-1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//commentaire
+/*
+ * ici totalement inutil
+ */
+int main(void)
+{
+	printf("Exercice, \"Programme de base\"\n\tHello World!");
+	return (0);
+}
